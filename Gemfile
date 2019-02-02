174901e1cc98647102512e03eabbbfed1a5343d1
Ahmed <ahmedshoukr2@gmail.com>
2019-02-01 20:07:32 -0800

    pagination start -must finish later

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
 
+gem 'will_paginate', '>= 3.1'
+
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,6 +2,16 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.all
+    # @places = Place.page(params[:page]) -- TRIED TO PAGINATE -- NOT WORKING
+    #all places in database stuffed into var @places
   end
+
+
+  # def index
+  #   @tasks = Task.search(params[:term], params[:page])
+  # end
+
+
+
 
 end
