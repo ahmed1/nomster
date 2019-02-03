579042ad1e9c2c703dca61a7feb65651e8ee97fc
Ahmed <ahmedshoukr2@gmail.com>
2019-02-02 21:02:46 -0800

    more places validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true #prevent any new place to be saved without a name
+  validates :name, presence: true, length: { minimum: 4}
+  validates :address, :description, presence: true #prevent any new place to be saved without a name
+
 end
