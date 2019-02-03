3d4ab76e68ad674a6d6e33cc07d2c247fbdda9bf
Ahmed <ahmedshoukr2@gmail.com>
2019-02-02 18:05:47 -0800

    details page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -19,6 +19,12 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
 
+
+  def show
+    @place = Place.find(params[:id])
+  end
+
+
   private
 
   def place_params #pulls values
